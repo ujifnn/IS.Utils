7a4a4121ca5ceeef5a95604a0277f13d04061597
Dmitry Vyakhirev <dmitry.vyakhirev@hotmail.com>
2013-07-31 21:45:28 +0400

    Version 0.1 submitted

---
+++ src/IS.Utils/IS.Utils.Common/Asserts/Assert.cs
@@ -0,0 +1,47 @@
+﻿/**********************************************************************************
+ *
+ * Copyright (C) 2013 - 2013 IT-Solutions. All rights reserved.
+ * 
+ *********************************************************************************/
+
+using System;
+
+namespace IS.Utils.Common.Asserts
+{
+    /// <summary>
+    /// Включает в себя методы валидации.
+    /// </summary>
+    /// <remarks>
+    /// Данный класс используется для валидации выражений и объектов, <b>не являющихся аргументами</b> методов. 
+    /// Для валидации аргументов рекомендуется использовать <see cref="AssertArg"/>.
+    /// <p><i>Не используйте данный класс в тестах.</i></p>
+    /// </remarks>
+    public static class Assert
+    {
+        /// <summary>
+        /// Бросает <see cref="NullReferenceException"/> если выражение равно <c>null</c>.
+        /// </summary>
+        /// <param name="obj">выражение</param>
+        /// <param name="errorMessage">текст сообщения об ошибке (по умолчанию <c>null</c>)</param>
+        public static void NotNull(object obj, string errorMessage = null)
+        {
+            if (obj == null)
+            {
+                throw new NullReferenceException(errorMessage);
+            }
+        }
+
+        /// <summary>
+        /// Бросает <see cref="InvalidOperationException"/> если условие равно <c>false</c>.
+        /// </summary>
+        /// <param name="condition">условие</param>
+        /// <param name="errorMessage">текст сообщения об ошибке (по умолчанию <c>null</c>)</param>
+        public static void IsTrue(bool condition, string errorMessage = null)
+        {
+            if (!condition)
+            {
+                throw new InvalidOperationException(errorMessage);
+            }
+        }
+    }
+}
---
+++ src/IS.Utils/IS.Utils.Common/Asserts/AssertArg.cs
@@ -0,0 +1,64 @@
+﻿/**********************************************************************************
+ *
+ * Copyright (C) 2013 - 2013 IT-Solutions. All rights reserved.
+ * 
+ *********************************************************************************/
+
+using System;
+
+namespace IS.Utils.Common.Asserts
+{
+    /// <summary>
+    /// Включает в себя методы валидации аргументов.
+    /// <br />
+    /// Данный класс полезен для валидации аргументов публичных методов других классов.
+    /// </summary>
+    /// <remarks>
+    /// Все бросаемые исключения - производные от <see cref="ArgumentException"/>.
+    /// <p><i>Не используйте данный класс в тестах.</i></p>
+    /// </remarks>
+    public static class AssertArg
+    {
+        /// <summary>
+        /// Бросает <see cref="ArgumentNullException"/> если аргумент равен <c>null</c>.
+        /// </summary>
+        /// <param name="argValue">значение аргумента</param>
+        /// <param name="argName">имя аргумента</param>
+        /// <param name="errorMessage">текст сообщения об ошибке (по умолчанию <c>null</c>)</param>
+        public static void NotNull(object argValue, string argName, string errorMessage = null)
+        {
+            if (argValue == null)
+            {
+                throw new ArgumentNullException(argName, errorMessage);
+            }
+        }
+
+        /// <summary>
+        /// Бросает <see cref="ArgumentException"/> если аргумент - <c>null</c> или пустая строка.
+        /// </summary>
+        /// <param name="argValue">значение аргумента</param>
+        /// <param name="argName">имя аргумента</param>
+        /// <param name="errorMessage">текст сообщения об ошибке (по умолчанию <c>null</c>)</param>
+        public static void HasText(string argValue, string argName, string errorMessage = null)
+        {
+            if (string.IsNullOrEmpty(argValue))
+            {
+                throw new ArgumentException(argName, errorMessage);
+            }
+        }
+
+        /// <summary>
+        /// Бросает <see cref="ArgumentException"/> если условие, наложенное на аргумент, равно <c>false</c>.
+        /// </summary>
+        /// <param name="condition">условие на аргумент</param>
+        /// <param name="argName">имя аргумента</param>
+        /// <param name="errorMessage">текст сообщения об ошибке (по умолчанию <c>null</c>)</param>
+        public static void IsTrue(bool condition, string argName, string errorMessage = null)
+        {
+            if (!condition)
+            {
+                throw new ArgumentException(argName, errorMessage);
+            }
+        }
+    }
+}
